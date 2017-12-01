7eefc5f02134f39d1304ce80c9f1c58d391e6c80
valof <valof@hotmail.com>
2017-11-30 21:35:28 -0800

    This is to finish the fetch past. There is one problem left.

--- Fetch/testFetch.js
+++ Fetch/testFetch.js
@@ -5,15 +5,21 @@
         foo:foo
     }
 
+    var myBody = {
+    id: 12345,
+    name: 'abc',
+    age: 21
+    };
+
     var initObject = {
         method: 'POST',
         headers: new Headers(),
         mode: 'cors',
-        body: "{}"
+        body: JSON.stringify(myBody)
     }
     function foo(fetch){
       //fetch() method used with an URL endpoint and an init object
-      return fetch("https://jsonplaceholder.typicode.com/posts", initObject)
+      return fetch("https://jsonplaceholder.typicode.com/posts/1", initObject)
           .then(function(result){ //result contains a Response object
             return result.json() //returns a promise containing JSON data extracted from the Response object
           }).then((result)=>{
