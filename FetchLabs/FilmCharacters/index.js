186146793e81cd834100634b654b4bd70d7a6930
valof <valof@hotmail.com>
2017-12-10 22:27:04 -0800

    Generators

---
+++ FetchLabs/FilmCharacters/index.js
@@ -0,0 +1,112 @@
+function addParameterToURL(location, param){
+  let _url = location;
+  _url += (_url.split('?')[1] ? '&':'?') + param;
+  return _url;
+}
+
+// var attributes = document.getElementById(idAttributes);
+// var table=document.createElement('TABLE');
+// table.border='1';
+// var tbdy=document.createElement('TBODY');
+// table.appendChild(tbdy);
+
+function traverse_it(obj){
+  if( typeof traverse_it.array == 'undefined' ) {
+    traverse_it.array = [];
+    traverse_it.counter = 0;
+    traverse_it.depth = 0;
+  }
+  for(var prop in obj){
+      if(typeof obj[prop]=='object'){
+          // object
+          traverse_it.array[traverse_it.counter]=traverse_it.depth == 0
+            ? document.getElementById('idFaceURL').value
+            : `${"&nbsp &nbsp".repeat(traverse_it.depth)} ${prop}`;
+          traverse_it.counter+=1;
+          traverse_it.depth+=1;
+          var tr=document.createElement('TR');
+          tr.innerHTML = traverse_it.array[traverse_it.counter-1];
+          tbdy.appendChild(tr);
+
+          traverse_it(obj[prop]);
+      }else{
+          // something else
+          const multi=function (char, count){}
+          traverse_it.array[traverse_it.counter]=`${"&nbsp &nbsp".repeat(traverse_it.depth)} ${prop} : ${obj[prop]}`;
+          var tr=document.createElement('TR');
+          tr.innerHTML = traverse_it.array[traverse_it.counter];
+          tbdy.appendChild(tr);
+          traverse_it.counter+=1;
+      }
+  }
+  traverse_it.depth-=1;
+}
+
+function* getIt(url){
+  let movie = yield fetch(url);
+}
+
+function getCharacters(url, film, characters){
+  film.innerHTML='';
+  characters.innerHTML='';
+  let movie=getIt(url);
+  let promise = movie.next().value;
+  promise.then((value)=>{
+    return value.json();
+  }).then((value)=>{
+    film.innerHTML=value.title;
+    return value.characters;
+  }).then((value)=>{
+    value.forEach((characterUrl)=>{
+      let character=getIt(characterUrl);
+      let promiseCharacter = character.next().value;
+      promiseCharacter.then((value)=>{
+        return value.json();
+      }).then((value)=>{
+        characters.innerHTML=characters.innerHTML + `${value.name}<br>`;
+      })
+    });
+  })
+}
+
+document.getElementById("idBtn").onclick = ()=> {
+  const filmID = document.getElementById("idFilmNumber").value;
+  const url = `https://swapi.co/api/films/${filmID}/`;
+  const characters = getCharacters(url, document.getElementById("idFilmName"), document.getElementById("idCharacters"));
+};
+
+  // const reqBody =
+  //     {
+  //       "url": url
+  //     };
+  // const myHeader =  new Headers({
+  //     'Content-Type': 'application/json',
+  //     'Ocp-Apim-Subscription-Key':'0742e1d614844b478839efa1a9d80023'
+  // });
+
+  // const initObject = {
+  //   method: 'POST',
+  //   body: JSON.stringify(reqBody),
+  //   headers: myHeader
+  // }
+
+  // const request = new Request(
+  //   endpoint,
+  //   initObject);
+
+  // fetch(request).then(function(response){
+  //   if(response.ok){
+  //         return response.json();
+  //     }
+  //   else{
+  //         return Promise.reject(new Error(response.statusText));
+  //   }
+  // }).then(function(response){
+  //    traverse_it(response);
+  //    document.getElementById("idAttributes").appendChild(table);
+  //     //`Face analysis: ${traverse_it.array[1]}`;
+  // }).catch(function(err){
+  //     alert(err);
+  //     document.getElementById("idAttributes").innerHTML = "";
+  // });
+// }
